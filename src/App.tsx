64ac8888876884aeae849b37cd16a90a5672dedc
Isai Reyes <anonymus1201a@gmail.com>
2026-08-22 15:10:22 -0600

    Actualizacion de Archivo Principal la Raiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import "./App.css";
+
 import AppRouter from "./routes/AppRouter";
 
 function App() {
-    return <AppRouter />;
+    return (
+        <div className="app">
+            <div className="app-content">
+                <AppRouter />
+            </div>
+        </div>
+    );
 }
 
 export default App;
